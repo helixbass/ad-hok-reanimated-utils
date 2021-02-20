6084ad906502bc9251a957eb67468a8b516f780d
Julian Rosse <julian@helixbass.net>
2021-02-20 17:12:03 -0500

    set up babel plugin tests

---
+++ src/__tests__/babel-plugin.ts
@@ -0,0 +1,18 @@
+import pluginTester from 'babel-plugin-tester'
+
+import plugin from '../babel-plugin'
+
+pluginTester({
+  plugin,
+  snapshot: true,
+  tests: [
+    `
+      const Component = flowMax(
+        addAnimatedStyle('style', ({opacity}) => () => ({
+          opacity,
+        })),
+        () => null
+      )
+    `
+  ]
+})
