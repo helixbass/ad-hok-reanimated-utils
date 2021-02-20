0556e506f1b1231b87fa6ac5d41f7bd0467d9d38
Julian Rosse <julian@helixbass.net>
2021-02-20 17:26:38 -0500

    addDerivedValues() test

--- src/__tests__/babel-plugin.ts
+++ src/__tests__/babel-plugin.ts
@@ -12,6 +12,11 @@ pluginTester({
         addAnimatedStyle('style', ({opacity}) => () => ({
           opacity,
         })),
+        addAnimatedStyle('style', ({opacity}) => () => {
+          return {
+            opacity
+          }
+        }),
         () => null
       )
     `,
@@ -20,6 +25,23 @@ pluginTester({
       const Component = flowMax(
         addDerivedValue('value', ({position}) => () => ({
           opacity: position.value * 0.1,
+        })),
+        addDerivedValue('value', ({position}) => () => {
+          return {
+            opacity: position.value * 0.1,
+          }
+        }),
+        () => null
+      )
+    `,
+    // addDerivedValues()
+    `
+      const Component = flowMax(
+        addDerivedValues(({position}) => ({
+          value: () => position.value * 0.1,
+          otherValue: () => {
+            return position.value * 0.2
+          }
         })),
         () => null
       )
